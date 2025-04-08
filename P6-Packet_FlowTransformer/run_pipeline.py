fe6f513eb5ae1846c18e7bec558391a6a075cc09
umarrashid <umarrashid@hotmail.dk>
2025-04-08 14:55:42 +0200

    abselute oathRO

--- P6-Packet_FlowTransformer/run_pipeline.py
+++ P6-Packet_FlowTransformer/run_pipeline.py
@@ -1,7 +1,7 @@
 from pipeline.process import first_pass, second_pass
 
 # Set these to your actual dataset and output path
-DATASET_DIR = "/ceph/project/P6-iot-flow-ids/dataset/raw_dataset/DatasetAnomaly"
+DATASET_DIR = "/ceph/project/P6-iot-flow-ids/dataset/raw_dataset/DatasetAnomaly/BruteForce"
 OUTPUT_FILE = "/ceph/project/P6-iot-flow-ids/dataset/"
 
 first_pass(DATASET_DIR)
