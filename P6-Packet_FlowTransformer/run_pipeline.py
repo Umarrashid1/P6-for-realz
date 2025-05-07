4be9cc3085b87354c022dd64e5fbc7987c123c0c
umarrashid <umarrashid@hotmail.dk>
2025-05-07 13:17:04 +0200

    new dataset

--- P6-Packet_FlowTransformer/run_pipeline.py
+++ P6-Packet_FlowTransformer/run_pipeline.py
@@ -1,14 +1,18 @@
 from pipeline.process import preprocess_flows_as_sequences
+import time, pathlib
+t0 = time.time()
 
 
 DATASET_DIR = '../../dataset/raw_dataset'
-OUTPUT_FILE = '../../dataset/packet.pt'
+OUTPUT_FILE = '../../dataset/dummy.pt'
+
 preprocess_flows_as_sequences(
     dataset_dir=DATASET_DIR,
     output_file=str(OUTPUT_FILE),
-    test_mode=False,
-    rows_per_file=2000
+    test_mode=True,
+    rows_per_file=10000
 )
-print("Pipeline completed successfully.")
 
+print("Pipeline completed successfully.")
+print("elapsed", time.time() - t0)
 
