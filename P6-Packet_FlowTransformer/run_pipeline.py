4eaf262532c2bb4449baa00303fe14b5749d67ce
umarrashid <umarrashid@hotmail.dk>
2025-05-08 14:48:30 +0200

    Run on whole dataset

--- P6-Packet_FlowTransformer/run_pipeline.py
+++ P6-Packet_FlowTransformer/run_pipeline.py
@@ -4,13 +4,13 @@ t0 = time.time()
 
 
 DATASET_DIR = '../../dataset/raw_dataset'
-OUTPUT_FILE = '../../dataset/dummy.pt'
+OUTPUT_FILE = '../../dataset/packet.pt'
 
 preprocess_flows_as_sequences(
     dataset_dir=DATASET_DIR,
     output_file=str(OUTPUT_FILE),
-    test_mode=True,
-    rows_per_file=10000
+    test_mode=False,
+    rows_per_file=0000
 )
 
 print("Pipeline completed successfully.")
