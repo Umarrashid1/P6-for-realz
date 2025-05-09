ff2aa7c7eb470d53cba36dc6dee5930e1ce509e7
umarrashid <umarrashid@hotmail.dk>
2025-05-09 12:04:04 +0200

    Added checkpointing

--- P6-Packet_FlowTransformer/run_pipeline.py
+++ P6-Packet_FlowTransformer/run_pipeline.py
@@ -9,8 +9,8 @@ OUTPUT_FILE = '../../dataset/packet.pt'
 preprocess_flows_as_sequences(
     dataset_dir=DATASET_DIR,
     output_file=str(OUTPUT_FILE),
-    test_mode=False,
-    rows_per_file=10000
+    test_mode=True,
+    rows_per_file=300000
 )
 
 print("Pipeline completed successfully.")
