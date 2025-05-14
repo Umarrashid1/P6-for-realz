50b00fc9c04f20ede2e5e075426a7cd7aed4bf29
umarrashid <umarrashid@hotmail.dk>
2025-05-14 13:14:01 +0200

    bug

--- P6-Packet_FlowTransformer/pipeline/process_flow.py
+++ P6-Packet_FlowTransformer/pipeline/process_flow.py
@@ -188,9 +188,7 @@ def preprocess_flow_vectors_from_csvs(  # Renamed main function
     loaded_dfs_with_paths = []
     # Using ThreadPoolExecutor for I/O bound task of loading files
     with concurrent.futures.ThreadPoolExecutor(max_workers=os.cpu_count() or 1) as tpool:
-        # io_utils.load_csv_file should return (DataFrame, file_path) or None
-        # It should load ALL columns initially, selection happens in process_single_flow_file
-        futures = [tpool.submit(io_utils.load_csv_to_df, fp, None, test_mode, rows_per_file) for fp in pending_files]
+        futures = [tpool.submit(io_utils.load_csv, fp, test_mode, rows_per_file) for fp in pending_files]
         for future in concurrent.futures.as_completed(futures):
             result = future.result()
             if result is not None:
