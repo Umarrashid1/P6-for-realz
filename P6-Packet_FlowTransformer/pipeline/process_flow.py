0ab451d58aab328ca595068e512e814df5b0437d
umarrashid <umarrashid@hotmail.dk>
2025-05-14 13:04:38 +0200

    refactor: update import statements to use relative paths in process_flow.py

--- P6-Packet_FlowTransformer/pipeline/process_flow.py
+++ P6-Packet_FlowTransformer/pipeline/process_flow.py
@@ -308,7 +308,7 @@ if __name__ == "__main__":
 
     # --- Configuration for direct run ---
     # **** USER: Adjust these paths and settings for your FLOW data ****
-    FLOW_DATA_INPUT_DIR = '../../dataset/roni/DatasetFlow',  # INPUT: Directory with raw FLOW CSVs
+    FLOW_DATA_INPUT_DIR = '../../dataset/roni/DatasetFlow'  # INPUT: Directory with raw FLOW CSVs
     FLOW_DATA_OUTPUT_PT = '../../dataset/processed_flows.pt'  # OUTPUT: Where the final .pt file will be saved
 
     # This should point to a category_mappings.json file generated for your FLOW features
